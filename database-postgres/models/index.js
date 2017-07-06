4b5cf096e0a16c87bb52f761d314dff4e407f811
Andrea Miralles <dremiralles@yahoo.com>
2017-07-05 19:08:21 -0700

    Cleanup
    * remove apikey file from gitignore
    
    * delete unused files- apis, saved-queries, users- from db models

--- database-postgres/models/index.js
+++ database-postgres/models/index.js
@@ -1,4 +1,1 @@
-module.exports.apis = require('./apis');
-module.exports.users = require('./users');
 module.exports.words = require('./words');
-module.exports.savedQueries = require('./saved-queries');

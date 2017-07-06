b16d561d5b0b64855553248c96f48becfae9431b
Jacob Sklar <jsklar.js@gmail.com>
2017-07-06 11:21:06 -0700

    added link for more info and made the response slightly prettier

--- react-client/src/components/Yelp.jsx
+++ react-client/src/components/Yelp.jsx
@@ -5,17 +5,20 @@ const Yelp = ({response}) => {
   return (
   	<div>
       <div className="ui segment">
-        <h2>I recommend: {response.data.name}</h2>
+        <h2>I recommend {response.data.name}</h2>
       </div>
       <div className="ui horizontal segments">
         <div className="ui segment small image">
-            <img src={response.data.image} height="300" width="300"/>
+            <img src={response.data.image} height="350" width="350"/>
         </div>
         <div className="ui segments">
         <div className="ui segment">Rating: {response.data.rating}</div>
         <div className="ui segment">Price: {response.data.price}</div>
         <div className="ui segment">Address: {response.data.address}</div>
-        <div className="ui segment">Phone: {response.data.phone}</div>
+        <div className="ui segment">Phone Number: {response.data.phone}</div>
+        <div className="ui segment">
+          <a href={response.data.website}>More Information about {response.data.name}</a>
+        </div>
         </div>
       </div>
     </div>

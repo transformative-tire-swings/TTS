0c463b18806d985b286d27a4631cfa4b9bb4da01
Bryant Huang <b.luv.sic@gmail.com>
2017-07-07 19:54:59 -0700

    add button to modal and styles

--- react-client/src/components/IntroModal.jsx
+++ react-client/src/components/IntroModal.jsx
@@ -1,28 +1,45 @@
-import React, { Component } from 'react';
-import { Button, Header, Icon, Modal } from 'semantic-ui-react';
+import React from 'react';
+import { Button, Header, Icon, Modal, Image } from 'semantic-ui-react';
 
-export default class IntroModal extends Component {
+export default class IntroModal extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      modalOpen: true
+    };
   }
 
+  handleClose() {
+    console.log('closing handler');
+    this.setState({
+      modalOpen: false,
+    });
+  }
   render() {
     return (
       <Modal
-        defaultOpen
-        closeOnDocumentClick={true}
+        // defaultOpen
+        open={this.state.modalOpen}
         basic
-        size='small'
+        size='fullscreen'
+        onClose={this.handleClose}
       >
-        <Header icon='wheelchair' content='What can you do?' />
-        <Modal.Content>
-          <h3>Directions</h3>
-          <h3>Weather</h3>
-          <h3>Food</h3>
-          <h3>GOATS</h3>
+        <Image width='100%' src='https://zortrax.com/wp-content/uploads/2016/06/wave.gif' />
+        <Modal.Header style={{textAlign: 'center', marginTop: '-300px'}}>Hello Marcus, what can you do?</Modal.Header>
+        <Modal.Content style={{margin: '0 auto', width: '30%'}}>
+          <Modal.Description>
+            <div>
+              <h3>Directions</h3>
+              <h3>Weather</h3>
+              <h3>Food Recommendations</h3>
+              <h3>GOATS</h3>
+            </div>
+          </Modal.Description>
         </Modal.Content>
         <Modal.Actions>
-
+          <Button color='green' onClick={this.handleClose.bind(this)} inverted>
+            <Icon name='checkmark' /> Got it
+          </Button>
         </Modal.Actions>
       </Modal>
     );

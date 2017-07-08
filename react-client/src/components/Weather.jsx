f0f50ac769ee13aa5a94df56fe9bdd3e024702ad
Jacob Sklar <jsklar.js@gmail.com>
2017-07-08 12:26:33 -0700

    made the weather basic

--- react-client/src/components/Weather.jsx
+++ react-client/src/components/Weather.jsx
@@ -2,32 +2,36 @@ import React from 'react';
 
 const Weather = ({response}) => {
   const contentStyle = {
-      marginBottom: '20px',
+      marginTop: '20px',
       fontSize: '20px', 
       borderStyle: 'none', 
       boxShadow: 'none', 
       wordWrap: 'normal', 
       wordBreak: 'normal', 
       whiteSpace: 'normal',
-      color: 'white',
+      color: 'black',
       fontWeight: 'normal',
-      textIndent: '10px'
+      textIndent: '10px',
+      textAlign: 'center'
   };
   return (
     <div>
-      <div className="ui centered grid" style={{color: 'white'}}>
-        <p style={{marginBottom: '50px'}}>{response.text}</p>
+      <div className="ui centered grid">
+        <div style={{color: 'white', marginBottom: '30px', textAlign: 'center'}} className="ui centered row">{response.text}</div>
+
+      <div className="ui centered six wide column">
+      <div className="ui centered row">
+      <div className="ui segment">
+      <div className="ui fluid image">
+        <img src={"http://openweathermap.org/img/w/" + response.data.weather[0].icon + ".png"}/>
       </div>
 
-      <div className="ui centered item">
-        <div className="ui small circular image">
-            <img src={"http://openweathermap.org/img/w/" + response.data.weather[0].icon + ".png"} height="150" width="150"/>
+        <div style={contentStyle}>{response.data.weather[0].description.split(' ').reduce((acc, val) => (acc + val[0].toUpperCase() + val.slice(1) + ' '), '')}</div>
+        <div style={contentStyle}>{Math.round(9/5 * (response.data.main.temp - 273) + 32)} &#8457;</div>
+        <div style={contentStyle}>Wind: {response.data.wind.speed} mph</div>
         </div>
-        <div className="ui content">
-        <div className="ui description" style={contentStyle}>{response.data.weather[0].description.split(' ').reduce((acc, val) => (acc + val[0].toUpperCase() + val.slice(1) + ' '), '')}</div>
-        <div className="ui description" style={contentStyle}>{Math.round(9/5 * (response.data.main.temp - 273) + 32)} &#8457;</div>
-        <div className="ui description" style={contentStyle}>Wind: {response.data.wind.speed} mph</div>
-        </div>
+      </div>
+      </div>
       </div>
     </div>
   );

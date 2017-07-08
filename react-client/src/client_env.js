c96247b41c2aac69db0d561fd23d7bf22972708b
Bryant Huang <b.luv.sic@gmail.com>
2017-07-08 15:25:12 -0700

    add modal tabs

--- react-client/src/client_env.js
+++ react-client/src/client_env.js
@@ -1,4 +1,4 @@
 module.exports.client_env = {
   houndify_clientID: 'eOklf1CJmFIeIpKrLMEg4g==',
-  GOOGLEMAPS_API_KEY: 'AIzaSyCxR8yOC-zE0CyTjZH69LpvqdkhWp_ate0'
+  googlemaps_api_key: 'AIzaSyCxR8yOC-zE0CyTjZH69LpvqdkhWp_ate0'
 };
